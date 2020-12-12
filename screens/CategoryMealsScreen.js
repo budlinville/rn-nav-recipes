a8d82cab1ba96e3d95137df88e92788ab2d61f5a
bud linville <budlinville@gmail.com>
2020-12-11 18:13:42 -0600

    Implemented through Category Meal Screen

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,21 +1,36 @@
 import React from 'react';
+import { render } from 'react-dom';
 import { View, Text, StyleSheet, Button } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 
-import { CATEGORIES } from '../data/dummy-data';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 const CategoryMealsScreen = props => {
+	const renderMealItem = itemData => {
+		return (
+			<MealItem
+				title={itemData.item.title}
+				duration={itemData.item.duration}
+				complexity={itemData.item.complexity}
+				affordability={itemData.item.affordability}
+				image={itemData.item.imageUrl}
+				onSelectMeal={() => {}}
+			/>
+		);
+	};
+
 	const catId = props.navigation.getParam('categoryId');
-	const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+	
+	const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0);
+
 	return (
 		<View style={styles.screen}>
-			<Text>The Category Meal Screen!</Text>
-			<Text>{selectedCategory.title}</Text>
-			<Button title="Go to Meal Detail!" onPress={()=>{
-				props.navigation.navigate("MealDetail");
-			}}/>
-			<Button title="Go Back" onPress={() => {
-				props.navigation.pop();
-			}} />
+			<FlatList
+				data={displayedMeals}
+				keyExtractor={item => item.id}	// unneccessary with modern RN.. now automatically looks for id field
+				renderItem={renderMealItem}
+				style={{width: '100%'}}/>
 		</View>
 	);
 };
@@ -23,7 +38,6 @@ const CategoryMealsScreen = props => {
 CategoryMealsScreen.navigationOptions = navigationData => {
 	const catId = navigationData.navigation.getParam('categoryId');
 	const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
-	console.log(selectedCategory);
 	return {
 		headerTitle: selectedCategory.title
 	}
@@ -33,7 +47,8 @@ const styles = StyleSheet.create({
 	screen: {
 		flex: 1,
 		justifyContent: 'center',
-		alignItems: 'center'
+		alignItems: 'center',
+		padding: 10
 	}
 });
 
